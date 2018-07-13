f02d6426373a1318ac7be81fb8ed33951fd2decf
Taylor Gulley <taylorlgulley@yahoo.com>
2018-07-13 11:53:53 -0500

    arrays

--- 07-13/main.js
+++ 07-13/main.js
@@ -42,3 +42,56 @@ function stop(direction, speed){
 stop("forward", 85);
 stop("forward", 65);
 stop("forward", 95);
+
+
+// Given the following set of data about chicken birth rates, you need to filter out the objects where the birth rate value is greater than 15 per 1000 chickens for that year. Store the good years in a new array named `bestYears`.
+
+const birthRates = [
+  { year: 1969, birthRate: 13}, 
+  { year: 1970, birthRate: 16}, 
+  { year: 1971, birthRate: 15}, 
+  { year: 1972, birthRate: 11}, 
+  { year: 1973, birthRate: 18}, 
+  { year: 1974, birthRate: 17}, 
+  { year: 1975, birthRate: 9}
+]
+
+const bestYears = [];
+
+// Option1
+// for (let i = 0; i < birthRates.length; i++) {
+//     const currentYear = birthRates [i];
+//     if (currentYear.birthRate > 15) {
+//         bestYears.push(currentYear);
+//     }
+// }
+
+// console.log(bestYears);
+
+//Option 2
+// birthRates.forEach(function (currentYear) {
+//     if (currentYear.birthRate > 15) {
+//         bestYears.push(currentYear);
+//     }
+// })
+
+// console.log(bestYears);
+
+//Option 3
+for (currentYear of birthRates) {
+    if (currentYear.birthRate > 15) {
+        bestYears.push(currentYear);
+    }
+}
+console.log(bestYears);
+
+//Example of accessing object properties
+const politician = {
+    platform: {
+        money: "I like it",
+        health: "I'm against it"
+    },
+    district: "Michigan 13"
+}
+
+console.log(politician.platform.money);
